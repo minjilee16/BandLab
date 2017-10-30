de85e40b73a402bcea514c7b9fdf594fdfe8c10c
Minji Lee <minjilee8816@gmail.com>
2017-10-29 22:00:40 -0700

    enable to group by userId

--- react-client/src/components/Post.jsx
+++ react-client/src/components/Post.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-var Post = ({id, title, body}) => (
+var Post = ({id, userId, title, body}) => (
   <div className="box">
     <div className="id">
       <b>ID:</b> {id}<br/>
+    </div>
+    <div className="userId">
+      <b>UserId:</b> {userId}<br/>
     </div>
     <div className="title">
     <b>Title:</b> {title}<br/>
--- react-client/src/components/Posts.jsx
+++ react-client/src/components/Posts.jsx
@@ -6,38 +6,21 @@ class Posts extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      // sortById: true,
-      // sort: false,
-      // sortByTitle: null,
-      data: null
+      data: null,
+      group: null
     }
   }
 
-  componentDidmount() {
-    console.log('checking')
-    console.log('datat::', this.props.passData);
-    this.setState({
-      data:this.props.passData
-    })
-  }
-
   sortByTitle() {
-    // console.log('checking', this.props.passData[0])
-
-    // let titleStorage =[];
     let data = this.props.passData;
-
-
     data.sort(function(a, b){
       if(a.title < b.title) return -1;
       if(a.title > b.title) return 1;
       return 0;
     });
-
     this.setState({
       data: data
     })
-    // console.log('data??', data);
   }
 
   sortById() {
@@ -64,15 +47,53 @@ class Posts extends React.Component {
     })
   }
 
+  selectGroup (e) {
+    this.setState({
+      group: e.target.value
+    })
+  }
+
+  groupByUserId() {
+    const selectedGroup = [];
+    this.props.passData.map( data => { 
+      Number(this.state.group) === data.userId ? selectedGroup.push(data) : null 
+    });
+    this.setState({
+      data:selectedGroup
+    })
+  }
+
   render () {
+    let posts = null;
+    if(this.state.data === null) {
+      posts = this.props.passData.map( (data, index) => 
+          <Post key={index} id={data.id} title={data.title} body={data.body} userId={data.userId} />
+      )
+    } else if (this.state.data !== null) {
+      posts = this.state.data.map( (data, index) => 
+          <Post key={index} id={data.id} title={data.title} body={data.body} userId={data.userId} />
+      )
+    }
+    let userId = [];
+    this.props.passData.map((data) => userId.push(data.userId) );
+    let deletedDuplicateId = userId.filter( (ele, index, self) => { return index === self.indexOf(ele) });
     return (
       <div>
-       <input type="button" className="sort" value="Sort by Title" onClick={this.sortByTitle.bind(this)}/>
-       <input type="button" className="sort" value="Sort by ID" onClick={this.sortById.bind(this)}/>
-       <input type="button" className="sort" value="Sort by Body" onClick={this.sortByBody.bind(this)}/>
-        {this.props.passData.map( (data, index) => 
-          <Post key={index} id={data.id} title={data.title} body={data.body} />
-        )}
+        <div className="select-group"> Group by userId 
+          <select onChange={this.selectGroup.bind(this)}> 
+          { 
+            deletedDuplicateId.map((num, index) => 
+            <option key={index} value={num}>{num}</option>)
+          }
+          </select>
+          <input className="group-serachbutton" type="button" value="Search!" onClick={this.groupByUserId.bind(this)}/>
+        </div>
+        <div className="sort-buttons">
+          <input type="button" className="sort" value="Sort by Title" onClick={this.sortByTitle.bind(this)}/>
+          <input type="button" className="sort" value="Sort by ID" onClick={this.sortById.bind(this)}/>
+          <input type="button" className="sort" value="Sort by Body" onClick={this.sortByBody.bind(this)}/>
+        </div>
+        {posts}
       </div>
     )
   }
